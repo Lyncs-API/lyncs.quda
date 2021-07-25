41cc03d8b536b4af13d487dff85c2891f0860b7e
Simone Bacchio <s.bacchio@gmail.com>
2021-07-25 23:32:02 +0300

    Adding enum tests

---
+++ test/test_enums.py
@@ -0,0 +1,20 @@
+from lyncs_quda import enums, Enum
+
+
+def test_enums():
+
+    for enum in dir(enums):
+        if enum.startswith("_"):
+            continue
+
+        enum = getattr(enums, enum)
+        assert issubclass(enum, Enum)
+
+        for key, val in enum.items():
+            assert key in enum
+            assert val in enum
+            assert enum[key] == key
+            assert enum[key] == val
+            assert enum[key] == enum[val]
+            assert str(enum[key]) == key
+            assert int(enum[key]) == int(enum[val]) == val
