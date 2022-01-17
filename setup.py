c815ad66e8b6bb86ac0621bfa79383bfd07129e0
Simone Bacchio <s.bacchio@gmail.com>
2022-01-17 10:42:36 +0200

    Improving setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 import sys
 import os
 from lyncs_setuptools import setup, CMakeExtension, find_package
-#from post_build import post_build
 
 exec(open("post_build.py").read())
 
 requirements = [
-    "cupy",
     "lyncs-cppyy",
     "lyncs-utils>=0.2.2",
     "appdirs",
 ]
+
+findCUDA = find_package("CUDA")
+if findCUDA["found"]:
+    requirements.append(f"cupy-cuda{findCUDA['version'].replace('.','')}")
+
+print("LYNCS_QUDA requirements:", *requirements, sep="\n")
+
 
 QUDA_CMAKE_ARGS = {
     "CMAKE_BUILD_TYPE": "RELEASE",
@@ -18,16 +23,15 @@ QUDA_CMAKE_ARGS = {
     "QUDA_BUILD_ALL_TESTS": "OFF",
     "QUDA_GPU_ARCH": os.environ.get("QUDA_GPU_ARCH", "sm_60"),
     "QUDA_FORCE_GAUGE": "ON",
-    "QUDA_MPI": "OFF",
+    "QUDA_MPI": os.environ.get("QUDA_MPI", "OFF"),
     "QUDA_MULTIGRID": "ON",
 }
 
 
 findMPI = find_package("MPI")
-if findMPI["cxx_found"]:
+if findMPI["cxx_found"] and os.environ.get("QUDA_MPI", None) in (None, "ON"):
     requirements.append("lyncs_mpi")
     QUDA_CMAKE_ARGS["QUDA_MPI"] = "ON"
-
 
 QUDA_CMAKE_ARGS = [key + "=" + val for key, val in QUDA_CMAKE_ARGS.items()]
 print("QUDA options:", *QUDA_CMAKE_ARGS, sep="\n")
